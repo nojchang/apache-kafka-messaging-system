5ae7df6b4220456773d70c9b0af960bd48d5b1a1
nojchang <69415781+nojchang@users.noreply.github.com>
2023-09-08 17:16:05 -0700

    Update KafkaApplication.java

--- java/com/example/kafkaexample/KafkaApplication.java
+++ java/com/example/kafkaexample/KafkaApplication.java
@@ -21,7 +21,7 @@ public class KafkaApplication {
         return args -> {
             //to show an example of how Kafka can read everything from the topic really fast with no issues
             for (int i = 0; i < 100; i++) {
-                kafkaTemplate.send("example", "hello this is Jon Chang :)" + i); //Sending the message "hello kafka :)" to the topic: example (within KafkaTopicConfig.java)
+                kafkaTemplate.send("example", "hello this is Jon Chang :)" + i); //Sending the message to the topic: example (within KafkaTopicConfig.java)
             }
         };
     }
